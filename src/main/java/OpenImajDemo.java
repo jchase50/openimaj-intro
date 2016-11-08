33e32e288d7f032166fe1be0984dd47b0b281632
Jonathan Chase <jchase@jkchase.com>
2016-11-08 00:19:57 -0500

    initial commit. Transparent pixel color.

--- src/main/java/OpenImajDemo.java
+++ src/main/java/OpenImajDemo.java
@@ -1,4 +1,4 @@
-import org.openimaj.image.Image;
+import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.MBFImage;
 import org.openimaj.video.Video;
 import org.openimaj.video.VideoDisplay;
@@ -10,13 +10,18 @@ import org.openimaj.video.capture.VideoCaptureException;
  * Created by jonathanchase on 11/7/16.
  */
 public class OpenImajDemo implements VideoDisplayListener<MBFImage> {
-    Video<MBFImage> video;
-    VideoDisplay<MBFImage> display;
+    private Video<MBFImage> video;
+    private VideoDisplay<MBFImage> display;
+    float flatTransparentColor;
+    private float transparentTolerance;
+    private MBFImage backgroundFrame;
 
-    public OpenImajDemo() throws VideoCaptureException {
+    public OpenImajDemo(Float[] transparentColor, Float transparentTolerance) throws VideoCaptureException {
         video = new VideoCapture(640, 480);
         display = VideoDisplay.createVideoDisplay(video);
         display.addVideoListener(this);
+        this.flatTransparentColor = flattenRGB(transparentColor);
+        this.transparentTolerance = transparentTolerance;
     }
 
     @Override
@@ -26,10 +31,41 @@ public class OpenImajDemo implements VideoDisplayListener<MBFImage> {
 
     @Override
     public void beforeUpdate(MBFImage frame) {
-        frame.fill(new Float[]{0f, 0f, 0f});
+        if(backgroundFrame == null){
+            backgroundFrame = frame.clone();
+            DisplayUtilities.display(backgroundFrame);
+            return;
+        }
+        replaceTransparentPixels(frame);
+    }
+
+    private void replaceTransparentPixels(MBFImage frame) {
+        for(int x = 0; x < frame.getWidth(); x++){
+            for(int y=0; y < frame.getHeight(); y++){
+                if(isTransparentPixel(frame.getPixel(x, y))){
+                    frame.setPixel(x, y, backgroundFrame.getPixel(x, y));
+                }
+            }
+        }
+    }
+
+    private boolean isTransparentPixel(Float[] pixel) {
+        return isWithinRange(flattenRGB(pixel), flatTransparentColor, transparentTolerance);
+    }
+
+    private boolean isWithinRange(float eval, float target, float tolerance) {
+        return target - tolerance <= eval && eval <= target + tolerance;
+    }
+
+    private float flattenRGB(Float[] rgbColor) {
+        float sum = 0;
+        for(int i=0; i< rgbColor.length; i++){
+            sum += rgbColor[i];
+        }
+        return sum / rgbColor.length;
     }
 
     public static void main (String[] args) throws VideoCaptureException {
-        OpenImajDemo sliceDemo = new OpenImajDemo();
+        OpenImajDemo sliceDemo = new OpenImajDemo(new Float[]{0f,0f,0f}, 0.15f);
     }
 }
