c9ca8cb24d4b387ca9881a9bc4e6672acd5552cc
Jonathan Chase <jchase@jkchase.com>
2016-11-07 23:11:22 -0500

    initial commit. Capture and display video from camera.

--- src/main/java/OpenImajDemo.java
+++ src/main/java/OpenImajDemo.java
@@ -1,8 +1,16 @@
-
+import org.openimaj.image.MBFImage;
+import org.openimaj.video.Video;
+import org.openimaj.video.VideoDisplay;
+import org.openimaj.video.capture.VideoCapture;
+import org.openimaj.video.capture.VideoCaptureException;
 
 /**
  * Created by jonathanchase on 11/7/16.
  */
 public class OpenImajDemo {
 
+    public static void main (String[] args) throws VideoCaptureException {
+        Video<MBFImage> video = new VideoCapture(640, 480);
+        VideoDisplay display = VideoDisplay.createVideoDisplay(video);
+    }
 }
